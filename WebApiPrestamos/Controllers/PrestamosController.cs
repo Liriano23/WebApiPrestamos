f3f5858fa5f90f606cddf3c8a54904a44f84bc6c
Enmanuel Gonzalez Liriano <enmanuel_gonzalez1@ucne.edu.do>
2020-07-04 11:40:23 -0400

    Add project files.

---
+++ WebApiPrestamos/Controllers/PrestamosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoPrestamo.Models;
+using ProyectoPrestamo.BLL;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiPrestamos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrestamosController : ControllerBase
+    {
+        // GET: api/<PrestamosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PrestamosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PrestamosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PrestamosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PrestamosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
