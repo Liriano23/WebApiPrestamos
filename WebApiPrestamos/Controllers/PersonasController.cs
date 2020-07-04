11ed5564797ab683a2911d43cb437e94330cb741
Enmanuel Gonzalez Liriano <enmanuel_gonzalez1@ucne.edu.do>
2020-07-04 12:13:05 -0400

    Creating API Controllers

---
+++ WebApiPrestamos/Controllers/PersonasController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoPrestamo.BLL;
+using ProyectoPrestamo.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiPrestamos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        // GET: api/Personas
+        [HttpGet]
+        public ActionResult<List<Personas>> Get()
+        {
+            return PersonasBLL.GetList(x=> true);
+        }
+
+        // GET: api/Personas/5
+        [HttpGet("{id}")]
+        public ActionResult<Personas> Get(int id)
+        {
+            return PersonasBLL.Buscar(id);
+        }
+
+        // POST: api/Personas
+        [HttpPost]
+        public void Post([FromBody] Personas personas)
+        {
+            PersonasBLL.Guardar(personas);
+        }
+
+        // PUT: api/Personas/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Personas personas)
+        {
+            PersonasBLL.Modificar(personas);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            PersonasBLL.Eliminar(id);
+        }
+    }
+}
--- WebApiPrestamos/Controllers/PrestamosController.cs
+++ WebApiPrestamos/Controllers/PrestamosController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoPrestamo.Models;
 using ProyectoPrestamo.BLL;
@@ -16,34 +14,37 @@ namespace WebApiPrestamos.Controllers
     {
         // GET: api/<PrestamosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<Prestamos>>Get()
         {
-            return new string[] { "value1", "value2" };
+            return PrestamosBLL.GetList(x=> true);
         }
 
         // GET api/<PrestamosController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Prestamos> Get(int id)
         {
-            return "value";
+            return PrestamosBLL.Buscar(id);
         }
 
         // POST api/<PrestamosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Prestamos prestamos)
         {
+            PrestamosBLL.Guardar(prestamos);
         }
 
         // PUT api/<PrestamosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Prestamos prestamos)
         {
+            PrestamosBLL.Modificar(prestamos);
         }
 
         // DELETE api/<PrestamosController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            PrestamosBLL.Eliminar(id);
         }
     }
 }
